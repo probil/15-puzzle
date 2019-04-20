b858979fc85984de9f5033ea7314717851c25e4b
Max Lyashuk <max@onsweb.nl>
2019-04-21 01:47:35 +0300

    feat(gameField): connect `gameField` store to the root store

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,13 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
 
+import gameField from './modules/game-field';
+
 Vue.use(Vuex);
 
 export default new Vuex.Store({
   strict: process.env.NODE_ENV !== 'production',
-  modules: {},
+  modules: {
+    gameField,
+  },
 });
