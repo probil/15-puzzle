0a4246c28640fb8ab92164caaeb153851bc853fd
Max Lyashuk <max@onsweb.nl>
2019-04-21 11:33:29 +0300

    feat(app): use `Moves` component

--- src/components/App.vue
+++ src/components/App.vue
@@ -1,26 +1,32 @@
 <template>
   <div class="app">
-    <PlayBox>
-      <GameField
-        :grid="grid"
-        @tile-clicked="moveTileByPoint"
-      ></GameField>
-    </PlayBox>
+    <div class="app__container">
+      <Moves :value="moves"></Moves>
+      <PlayBox>
+        <GameField
+          :grid="grid"
+          @tile-clicked="moveTileByPoint"
+        ></GameField>
+      </PlayBox>
+    </div>
   </div>
 </template>
 <script>
   import { mapGetters, mapActions } from 'vuex'
   import PlayBox from './PlayBox'
   import GameField from './GameField'
+  import Moves from './Moves'
 
   export default {
     components: {
       PlayBox,
       GameField,
+      Moves,
     },
     computed: {
       ...mapGetters(['isGameStarted']),
       ...mapGetters('gameField', ['grid']),
+      ...mapGetters('moves', ['moves']),
     },
     methods: {
       ...mapActions(['startNewGame', 'moveTileByPoint']),
@@ -39,4 +45,7 @@
     display flex
     justify-content center
     align-items center
+
+    &__container
+      margin 1rem
 </style>
