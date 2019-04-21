a7a128ccc4c421c00f96a3e0335a4ff9f037842b
Max Lyashuk <max@onsweb.nl>
2019-04-21 18:53:32 +0300

    refactor(timer): move `toMMSS` filter to the filters directory

---
+++ src/filters/index.js
@@ -0,0 +1,17 @@
+/* eslint-disable import/prefer-default-export */
+
+/**
+ * @param {Number} number
+ * @returns {String}
+ */
+const pad = number => (`0${number}`).slice(-2);
+
+/**
+ * @param {Number} value
+ * @return {string}
+ */
+export const toMMSS = (value) => {
+  const minutes = Math.trunc(value / 60);
+  const seconds = value % 60;
+  return `${pad(minutes)}:${pad(seconds)}`;
+};
