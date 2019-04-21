f687a6b2be0b49b8bba67c0ed67a42491378f98c
Max Lyashuk <max@onsweb.nl>
2019-04-21 16:45:59 +0300

    feat(overlay): implement `Start the game` overlay component

---
+++ src/components/OverlayStartTheGame.vue
@@ -0,0 +1,19 @@
+<template>
+  <OverlayBase>
+    Press <button class="btn-start" @click="startNewGame">Play</button> to start
+  </OverlayBase>
+</template>
+<script>
+  import OverlayBase from './OverlayBase'
+
+  export default {
+    components: {
+      OverlayBase,
+    },
+    methods: {
+      startNewGame() {
+        return this.$emit('start-game');
+      }
+    }
+  }
+</script>
