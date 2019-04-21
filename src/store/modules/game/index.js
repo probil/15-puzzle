3bac73096c913f0a7fc4d96609a75d10f89f58e0
Max Lyashuk <max@onsweb.nl>
2019-04-21 11:02:31 +0300

    feat(game): implement simple game store

---
+++ src/store/modules/game/index.js
@@ -0,0 +1,37 @@
+/* eslint-disable no-shadow, no-param-reassign */
+
+const state = {
+  isStarted: false,
+  isFinished: false,
+};
+
+const getters = {
+  isGameStarted: state => state.isStarted,
+};
+
+const mutations = {
+  startNewGame(state) {
+    state.isStarted = true;
+    state.isFinished = false;
+  },
+};
+
+const actions = {
+  startNewGame({ commit, dispatch }) {
+    dispatch('gameField/init');
+    dispatch('moves/reset');
+    commit('startNewGame');
+  },
+  moveTileByPoint({ dispatch }, point) {
+    const isMoved = dispatch('gameField/tryToMoveTileByPoint', point);
+    if (!isMoved) return;
+    dispatch('moves/increment');
+  },
+};
+
+export default {
+  state,
+  getters,
+  actions,
+  mutations,
+};
