2d68375c6f8b018d4d8f242b4404f64d3e5c017a
Max Lyashuk <max@onsweb.nl>
2019-04-21 11:54:47 +0300

    feat(infobar): implement simple infobar component

---
+++ src/components/InfoBar.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="info-bar">
+    <div class="info-bar__left">
+      <slot name="left"></slot>
+    </div>
+    <div class="info-bar__right">
+      <slot name="right"></slot>
+    </div>
+  </div>
+</template>
+<style lang="stylus">
+  .info-bar
+    height 30px
+    display flex
+    font-size 1.2rem
+
+    &__left
+      flex-basis 50%
+
+    &__right
+      flex-basis 50%
+</style>
